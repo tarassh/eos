1ab6727a74ec3ff5b678f4fa5728d48184814509
Taras Shchybovyk <tshchybo@gmail.com>
2018-06-11 10:35:26 +0300

    Initial commit for ledger wallet plugin.

---
+++ plugins/ledger_wallet_plugin/include/eosio/ledger_wallet_plugin/ledger_wallet_plugin.hpp
@@ -0,0 +1,36 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#pragma once
+
+#include <appbase/application.hpp>
+
+namespace eosio {
+
+    using namespace appbase;
+
+/**
+ *  This is a template plugin, intended to serve as a starting point for making new plugins
+ */
+    class ledger_wallet_plugin : public appbase::plugin<ledger_wallet_plugin> {
+    public:
+        ledger_wallet_plugin();
+
+        virtual ~ledger_wallet_plugin();
+
+        APPBASE_PLUGIN_REQUIRES()
+
+        virtual void set_program_options(options_description &, options_description &cfg) override;
+
+        void plugin_initialize(const variables_map &options);
+
+        void plugin_startup();
+
+        void plugin_shutdown();
+
+    private:
+        std::unique_ptr<class ledger_wallet_plugin_impl> my;
+    };
+
+}
---
+++ plugins/ledger_wallet_plugin/ledger_wallet_plugin.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include <eosio/ledger_wallet_plugin/ledger_wallet_plugin.hpp>
+
+namespace eosio {
+   static appbase::abstract_plugin& _ledger_wallet_plugin = app().register_plugin<ledger_wallet_plugin>();
+
+class ledger_wallet_plugin_impl {
+   public:
+};
+
+ledger_wallet_plugin::ledger_wallet_plugin():my(new ledger_wallet_plugin_impl()){}
+ledger_wallet_plugin::~ledger_wallet_plugin(){}
+
+void ledger_wallet_plugin::set_program_options(options_description&, options_description& cfg) {
+   cfg.add_options()
+         ("option-name", bpo::value<string>()->default_value("default value"),
+          "Option Description")
+         ;
+}
+
+void ledger_wallet_plugin::plugin_initialize(const variables_map& options) {
+   if(options.count("option-name")) {
+      // Handle the option
+   }
+}
+
+void ledger_wallet_plugin::plugin_startup() {
+   // Make the magic happen
+}
+
+void ledger_wallet_plugin::plugin_shutdown() {
+   // OK, that's enough magic
+}
+
+}
