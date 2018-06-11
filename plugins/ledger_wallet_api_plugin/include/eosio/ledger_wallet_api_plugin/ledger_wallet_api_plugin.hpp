c0cf2e987dbf9ff2e8e23fdb8953dfa28c62c96a
Taras Shchybovyk <tshchybo@gmail.com>
2018-06-11 16:24:22 +0300

    Add ledger wallet api http endpoints.

--- plugins/ledger_wallet_api_plugin/include/eosio/ledger_wallet_api_plugin/ledger_wallet_api_plugin.hpp
+++ plugins/ledger_wallet_api_plugin/include/eosio/ledger_wallet_api_plugin/ledger_wallet_api_plugin.hpp
@@ -4,6 +4,8 @@
  */
 #pragma once
 #include <appbase/application.hpp>
+#include <eosio/ledger_wallet_plugin/ledger_wallet_plugin.hpp>
+#include <eosio/http_plugin/http_plugin.hpp>
 
 namespace eosio {
 
@@ -14,10 +16,14 @@ using namespace appbase;
  */
 class ledger_wallet_api_plugin : public appbase::plugin<ledger_wallet_api_plugin> {
 public:
-    ledger_wallet_api_plugin();
-   virtual ~ledger_wallet_api_plugin();
+    ledger_wallet_api_plugin() = default;
+    ledger_wallet_api_plugin(const ledger_wallet_api_plugin&) = delete;
+    ledger_wallet_api_plugin(ledger_wallet_api_plugin&&) = delete;
+    ledger_wallet_api_plugin& operator=(const ledger_wallet_api_plugin&) = delete;
+    ledger_wallet_api_plugin& operator=(ledger_wallet_api_plugin&&) = delete;
+    virtual ~ledger_wallet_api_plugin() override = default;
  
-   APPBASE_PLUGIN_REQUIRES()
+   APPBASE_PLUGIN_REQUIRES((ledger_wallet_plugin) (http_plugin))
    virtual void set_program_options(options_description&, options_description& cfg) override;
  
    void plugin_initialize(const variables_map& options);
@@ -25,7 +31,6 @@ public:
    void plugin_shutdown();
 
 private:
-   std::unique_ptr<class ledger_wallet_api_plugin_impl> my;
 };
 
 }
