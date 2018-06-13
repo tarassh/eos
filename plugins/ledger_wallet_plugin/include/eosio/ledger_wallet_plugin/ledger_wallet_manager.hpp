d4072ef64450b5e7f39920e51efd69125fd22341
Taras Shchybovyk <tshchybo@gmail.com>
2018-06-13 18:30:15 +0300

    Add exception for unused commands.

--- plugins/ledger_wallet_plugin/include/eosio/ledger_wallet_plugin/ledger_wallet_manager.hpp
+++ plugins/ledger_wallet_plugin/include/eosio/ledger_wallet_plugin/ledger_wallet_manager.hpp
@@ -24,18 +24,18 @@ public:
 
     /// Set the path for location of wallet files.
     /// @param p path to override default ./ location of wallet files.
-    void set_dir(const boost::filesystem::path& p) { }
+    void set_dir(const boost::filesystem::path& p);
 
     /// Set the timeout for locking all wallets.
     /// If set then after t seconds of inactivity then lock_all().
     /// Activity is defined as any wallet_manager method call below.
-    void set_timeout(const std::chrono::seconds& t) { }
+    void set_timeout(const std::chrono::seconds& t);
 
     /// @see wallet_manager::set_timeout(const std::chrono::seconds& t)
     /// @param secs The timeout in seconds.
-    void set_timeout(int64_t secs) { }
+    void set_timeout(int64_t secs);
 
-    void set_eosio_key(const std::string& key) { }
+    void set_eosio_key(const std::string& key);
 
     /// Sign transaction with the private keys specified via their public keys.
     /// Use chain_controller::get_required_keys to determine which keys are needed for txn.
@@ -62,38 +62,38 @@ public:
     /// @return Plaintext password that is needed to unlock wallet. Caller is responsible for saving password otherwise
     ///         they will not be able to unlock their wallet. Note user supplied passwords are not supported.
     /// @throws fc::exception if wallet with name already exists (or filename already exists)
-    std::string create(const std::string& name) { return ""; }
+    std::string create(const std::string& name);
 
     /// Open an existing wallet file dir/{name}.wallet.
     /// Note this does not unlock the wallet, see wallet_manager::unlock.
     /// @param name of the wallet file (minus ext .wallet) to open.
     /// @throws fc::exception if unable to find/open the wallet file.
-    void open(const std::string& name) {}
+    void open(const std::string& name);
 
     /// @return A list of wallet names with " *" appended if the wallet is unlocked.
-    std::vector<std::string> list_wallets() { return vector<std::string>(); }
+    std::vector<std::string> list_wallets();
 
     /// @return A list of private keys from a wallet provided password is correct to said wallet
-    map<public_key_type,private_key_type> list_keys(const string& name, const string& pw) { return map<public_key_type, private_key_type>(); };
+    map<public_key_type,private_key_type> list_keys(const string& name, const string& pw);
 
     /// @return A set of public keys from all unlocked wallets, use with chain_controller::get_required_keys.
     flat_set<public_key_type> get_public_keys();
 
     /// Locks all the unlocked wallets.
-    void lock_all() {}
+    void lock_all();
 
     /// Lock the specified wallet.
     /// No-op if wallet already locked.
     /// @param name the name of the wallet to lock.
     /// @throws fc::exception if wallet with name not found.
-    void lock(const std::string& name) {}
+    void lock(const std::string& name);
 
     /// Unlock the specified wallet.
     /// The wallet remains unlocked until ::lock is called or program exit.
     /// @param name the name of the wallet to lock.
     /// @param password the plaintext password returned from ::create.
     /// @throws fc::exception if wallet not found or invalid password or already unlocked.
-    void unlock(const std::string& name, const std::string& password) {}
+    void unlock(const std::string& name, const std::string& password);
 
     /// Import private key into specified wallet.
     /// Imports a WIF Private Key into specified wallet.
@@ -101,7 +101,7 @@ public:
     /// @param name the name of the wallet to import into.
     /// @param wif_key the WIF Private Key to import, e.g. 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
     /// @throws fc::exception if wallet not found or locked.
-    void import_key(const std::string& name, const std::string& wif_key) {}
+    void import_key(const std::string& name, const std::string& wif_key);
 
     /// Creates a key within the specified wallet.
     /// Wallet must be opened and unlocked
@@ -109,7 +109,7 @@ public:
     /// @param type of key to create
     /// @throws fc::exception if wallet not found or locked, or if the wallet cannot create said type of key
     /// @return The public key of the created key
-    string create_key(const std::string& name, const std::string& key_type) { return ""; }
+    string create_key(const std::string& name, const std::string& key_type);
 
 private:
 
